f64c4258818723ea36d32199d3a9c0f06a0e8cff
josearivas <rivass.josea@gmail.com>
2021-04-01 07:11:15 -0400

    se remueve j2 Dockerfile

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 rm -rf logs
 j2 config/odoo.conf.j2 env.json  > config/odoo.conf
-j2 Dockerfile.j2 env.json > Dockerfile
 j2 docker-compose.yml.j2 env.json > docker-compose.yml
 docker-compose up -d
