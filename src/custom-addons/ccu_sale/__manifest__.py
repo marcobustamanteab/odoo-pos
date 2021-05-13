d2c226df10b5922036b17e21176a0c07e9e8621c
ezeladat <PeLaO1840>
2021-05-12 23:40:35 -0400

    primera modificacion transaction_id

--- src/custom-addons/ccu_sale/__manifest__.py
+++ src/custom-addons/ccu_sale/__manifest__.py
@@ -13,6 +13,7 @@
     "depends": [
         "sale",
         "ccu_base",
+        "point_of_sale"
     ],
     'data': [
         # "security/ir.model.access.csv",
@@ -22,7 +23,11 @@
         # "views/product_property.xml",
         # "views/product_tag.xml",
         # "views/product_template_only.xml",
+        'views/PaymentLinesValidator.xml'
     ],
     "application": True,
     "sequence": 0,
+    'qweb': [
+        'static/src/xml/PaymentLinesValidator.xml'
+    ]
 }
---
+++ src/custom-addons/ccu_sale/static/src/js/PaymentLinesValidator.js
@@ -0,0 +1,55 @@
+odoo.define('ccu_sale.PaymentTransbankLinesValidator', function (require) {
+    "use strict";
+
+    const { xml } = owl.tags;
+    const PaymentScreenStatus = require('point_of_sale.PaymentScreenPaymentLines');
+    const PaymentScreen = require('point_of_sale.PaymentScreen');
+    const NumberBufferTrx = require('point_of_sale.NumberBuffer');
+    const Registries = require('point_of_sale.Registries');
+
+    const PaymentTransbankLinesValidator = PaymentScreenPaymentLines =>
+        class extends PaymentScreenPaymentLines {
+            constructor() {
+                super(...arguments);
+                useListener('save-payment-line', this.savePaymentLine);
+                useListener('edit-payment-line', this.editPaymentLine);
+                useListener('update-selected-paymentline', this.updateTransbankPaymentline);
+                NumberBufferTrx.use({
+                    // The numberBuffer listens to this event to update its state.
+                    // Basically means 'update the buffer when this event is triggered'
+                    nonKeyboardInputEvent: 'input-from-numpad',
+                    // When the buffer is updated, trigger this event.
+                    // Note that the component listens to it.
+                    triggerAtInput: 'update-selected-paymentline',
+                });
+            }
+
+            savePaymentLine(event) {
+                const {cid} = event.detail;
+                const line = this.paymentLines.find((line) => line.cid === cid);
+                line.transbank_saved = true
+                line.transaction_id = NumberBufferTrx.getText();
+
+                NumberBuffer.reset();
+                this.render();
+            }
+
+            editPaymentLine(event) {
+                const {cid} = event.detail;
+                const line = this.paymentLines.find((line) => line.cid === cid);
+                line.transbank_saved = false
+                line.transaction_id = NumberBufferTrx.getText();
+
+                NumberBuffer.reset();
+                this.render();
+            }
+
+            updateTransbankPaymentline() {
+                line.transaction_id = NumberBufferTrx.getText();
+                this.render();
+            }
+        }
+
+    Registries.Component.extend(PaymentScreenPaymentLines, PaymentTransbankLinesValidator);
+
+});
