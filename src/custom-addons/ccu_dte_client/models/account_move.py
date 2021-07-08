75ab4930638536ddf3d0baf5b3908feafc927a2c
Martin Salcedo <martinsalcedo.apps@gmail.com>
2021-07-08 18:47:39 -0400

    [FIX] default_code and etd_code with value it empty

--- src/custom-addons/ccu_dte_client/models/account_move.py
+++ src/custom-addons/ccu_dte_client/models/account_move.py
@@ -78,13 +78,13 @@ class AccountMove(models.Model):
         }
         for invoice_line in self.invoice_line_ids:
             ivals = {}
-            ivals["product_id"] = invoice_line.product_id.default_code
+            ivals["product_id"] = invoice_line.product_id.default_code or "000000"
             ivals["quantity"] = invoice_line.quantity
             ivals["price_unit"] = invoice_line.price_unit
             ivals["name"] = invoice_line.name or invoice_line.product_id.name
             ivals["account_id"] = invoice_line.account_id.code
-            ivals["display_type"] = "product"
-            ivals["ref_etd"] = invoice_line.product_id.default_code
+            # ivals["display_type"] = "product"
+            ivals["ref_etd"] = invoice_line.product_id.default_code or "000000"
             ivals["discount"] = invoice_line.discount
             ivals["invoice_line_tax_ids"] = ",".join([x.dte_service_code or 'ERR' for x in invoice_line.tax_ids])
             dte_to_send["DETAIL"].append(ivals)
