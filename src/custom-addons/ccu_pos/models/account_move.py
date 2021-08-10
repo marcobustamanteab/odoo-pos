6b530ee934042ae8bf147a02b3fdb14ed0380f45
Martin Salcedo <martinsalcedo.apps@gmail.com>
2021-08-10 15:20:14 -0400

    [FIX] Field name mistake

--- src/custom-addons/ccu_pos/models/account_move.py
+++ src/custom-addons/ccu_pos/models/account_move.py
@@ -7,7 +7,7 @@ class AccountMove(models.Model):
     _inherit = ['account.move']
 
     def _default_sequence_prefix(self):
-        if len(self.pos_prder_ids) > 0:
+        if len(self.pos_order_ids) > 0:
             pos_order = self.pos_order_ids[0]
             prefix = pos_order.session_id.config_id.sequence_id.prefix
             return prefix.strip('/') if prefix else 'XXXXX'
@@ -18,7 +18,7 @@ class AccountMove(models.Model):
 
     def _compute_pos_sequence_prefix(self):
         for rec in self:
-            if len(rec.pos_prder_ids) > 0:
+            if len(rec.pos_order_ids) > 0:
                 pos_order = rec.pos_order_ids[0]
                 prefix = pos_order.session_id.config_id.sequence_id.prefix
                 rec.pos_sequence_prefix = prefix.strip('/') if prefix else 'XXXXX'
