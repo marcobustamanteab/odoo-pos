6ac36f10c88cc35d9ef01c83ab9d12b421eeee62
Daniel <daniel.claveria@gmail.com>
2021-08-17 14:35:00 -0400

    implemento RUT y SAP Code Genéricos

--- src/custom-addons/ccu_connector_esb_account_out/models/res_partner.py
+++ src/custom-addons/ccu_connector_esb_account_out/models/res_partner.py
@@ -6,3 +6,6 @@ class ResPartner(models.Model):
 
     cost_center_code = fields.Char(string='Cost Center Code', index=True)
     sap_code = fields.Char(string='SAP Client Code', index=True)
+    use_generic_sap_client = fields.Boolean(string='Use generic SAP Client')
+    generic_sap_code = fields.Char(string='Generic SAP Code')
+    generic_RUT = fields.Char(string='Generic SAP RUT')
