f077e098b7c1587e9d6e3dc684db0690ff8883b4
Martin Salcedo <martinsalcedo.apps@gmail.com>
2021-08-27 08:26:54 -0400

    [FIX] Bank Statement FIX

--- src/custom-addons/ccu_pos/models/account_bank_statement.py
+++ src/custom-addons/ccu_pos/models/account_bank_statement.py
@@ -10,3 +10,9 @@ class AccountBankStatementLine(models.Model):
         vals = super(AccountBankStatementLine, self)._prepare_liquidity_move_line_vals()
         vals['pos_order_id'] = self.pos_order_id.id
         return vals
+
+    @api.model
+    def _prepare_counterpart_move_line_vals(self, counterpart_vals, move_line=None):
+        vals = super(AccountBankStatementLine, self)._prepare_counterpart_move_line_vals(counterpart_vals, move_line)
+        vals['pos_order_id'] = self.pos_order_id.id
+        return vals
