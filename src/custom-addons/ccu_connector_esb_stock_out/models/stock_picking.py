c275f53a8160fee46871a23a8ea1a0627cb74cc3
Martin Salcedo <martinsalcedo.apps@gmail.com>
2021-08-30 16:58:11 -0400

    [FIX] ALLOCNBR with new value

--- src/custom-addons/ccu_connector_esb_stock_out/models/stock_picking.py
+++ src/custom-addons/ccu_connector_esb_stock_out/models/stock_picking.py
@@ -105,7 +105,7 @@ class StockPicking(models.Model):
                         "stge_loc": almacen,  # Almacen SAP/ubicación Odoo
                         "move_stloc": "0",
                         "batch": "NONE",
-                        "ALLOCNBR": line.picking_id.origin,
+                        "ALLOCNBR": self.pos_order_id.name or self.name or '',
                         "entry_qnt": line.qty_done,
                         "item_text": line.picking_id.origin,
                         "move_type": line.move_id.picking_type_id.ccu_code_usage
