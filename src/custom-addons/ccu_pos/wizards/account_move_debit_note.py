01f4b8fd9bc2259e1370a5f92a0a44c023a33ebe
Martin Salcedo <martinsalcedo.apps@gmail.com>
2021-08-31 17:26:42 -0400

    [FIX] Recompute taxes

--- src/custom-addons/ccu_pos/wizards/account_move_debit_note.py
+++ src/custom-addons/ccu_pos/wizards/account_move_debit_note.py
@@ -25,13 +25,39 @@ class AccountDebitNote(models.TransientModel):
             res.update({'journal_id':journals.id})
         return res
 
+    #Redefined
     def create_debit(self):
         self.ensure_one()
-        for move in self.move_ids:
+        new_moves = self.env['account.move']
+        for move in self.move_ids.with_context(include_business_fields=True): #copy sale/purchase links
             if move.move_type == 'out_invoice' and self.l10n_cl_edi_reference_doc_code in ('1', '2'):
                 raise UserError(
                     "Código de Motivo %s no disponible para este comprobante" % (self.l10n_cl_edi_reference_doc_code))
             if move.move_type == 'out_refund' and self.l10n_cl_edi_reference_doc_code in ('2', '3'):
                 raise UserError(
                     "Código de Motivo %s no disponible para este comprobante" % (self.l10n_cl_edi_reference_doc_code))
-        return super(AccountDebitNote, self).create_debit()
+            default_values = self._prepare_default_values(move)
+            new_move = move.copy(default=default_values)
+            move_msg = _(
+                "This debit note was created from:") + " <a href=# data-oe-model=account.move data-oe-id=%d>%s</a>" % (
+                       move.id, move.name)
+            new_move.message_post(body=move_msg)
+            new_move._recompute_dynamic_lines(recompute_all_taxes=True)
+            new_moves |= new_move
+
+        action = {
+            'name': _('Debit Notes'),
+            'type': 'ir.actions.act_window',
+            'res_model': 'account.move',
+            }
+        if len(new_moves) == 1:
+            action.update({
+                'view_mode': 'form',
+                'res_id': new_moves.id,
+            })
+        else:
+            action.update({
+                'view_mode': 'tree,form',
+                'domain': [('id', 'in', new_moves.ids)],
+            })
+        return action
