7b1029e7fcc5bf036aa17682a24a19cbd0d5eb9e
ezeladat <ezeladat>
2021-12-14 07:46:52 -0300

    correccion formato

--- src/custom-addons/ccu_sale_book_report/wizards/etd_account_report.py
+++ src/custom-addons/ccu_sale_book_report/wizards/etd_account_report.py
@@ -232,13 +232,13 @@ class etd_account_excel_wizard_form(models.TransientModel):
                 # json_txt = json.loads(line.posted_payload)
                 # _logger.info('line.posted_payload -> ' + assent['DOCUMENT_POST']['ASSENT'])
                 cebe = ''
-                if line.posted_payload is not False:
-                    assent = line.posted_payload
-                    _logger.info('line.posted_payload -> ' + assent['DOCUMENT_POST']['ASSENT'])
-                    for iter in assent['DOCUMENT_POST']['ASSENT']:
-                        if iter == line.product_id.code:
-                            _logger.info('CEBE' + iter['CEBE'])
-                            cebe = iter['CEBE']
+                # if line.posted_payload is not False:
+                #     assent = line.posted_payload
+                #     _logger.info('line.posted_payload -> ' + assent['DOCUMENT_POST']['ASSENT'])
+                #     for iter in assent['DOCUMENT_POST']['ASSENT']:
+                #         if iter == line.product_id.code:
+                #             _logger.info('CEBE' + iter['CEBE'])
+                #             cebe = iter['CEBE']
 
                 worksheet.write(n, 37, cebe or '', style)
                 worksheet.write(n, 38, rec.currency_id.name or '', style)
