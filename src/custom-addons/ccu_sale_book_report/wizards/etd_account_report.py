6fd4ec8cd1057bc5c2c3e6529fe2e36b762e1caa
ezeladat <ezeladat>
2021-12-23 21:01:15 -0300

    debug data

--- src/custom-addons/ccu_sale_book_report/wizards/etd_account_report.py
+++ src/custom-addons/ccu_sale_book_report/wizards/etd_account_report.py
@@ -246,8 +246,8 @@ class etd_account_excel_wizard_form(models.TransientModel):
                 cebe = ''
                 if line.posted_payload is not False:
                     assent = line.posted_payload
-                    # _logger.info('line.posted_payload -> ' + assent)
-                    # _logger.info(type(line.posted_payload))
+                    _logger.info(line.posted_payload)
+                    _logger.info(type(line.posted_payload))
                     assent_tmp = assent['DOCUMENT_POST']['ASSENT']
                     _logger.info('largo -> ' + str(len(assent_tmp)))
                     for iterf in assent_tmp:
