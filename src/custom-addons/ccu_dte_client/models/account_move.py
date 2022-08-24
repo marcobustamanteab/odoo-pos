adca6f5192785ee54beb338432d76982b9e776cb
Daniel <daniel.claveria@gmail.com>
2022-08-24 12:53:08 -0400

    validación sólo para Facturas

--- src/custom-addons/ccu_dte_client/models/account_move.py
+++ src/custom-addons/ccu_dte_client/models/account_move.py
@@ -76,14 +76,11 @@ class AccountMove(models.Model):
                 msg = "DTE Client Configuration Missing: Company (%s)" % (move.company_id.name)
                 _logger.warning(msg)
                 raise UserError(msg)
-            if not move.partner_id.city_id:
+            if not move.partner_id.city_id and move.l10n_latam_document_type_id.code not in [39]:
                 msg = "Partner city fields is incomplete (partner: %s)" % (move.partner_id.name)
                 _logger.warning(msg)
                 raise UserError(msg)
-            if not move.partner_id.state_id.country_id.code or not move.partner_id.state_id.code:
-                msg = "Partner Data Error: undefined city o state (partner: %s)" % (move.partner_id.name)
-                _logger.warning(msg)
-                raise UserError(msg)
+
             if not config.enabled:
                 _logger.info("DTE Synchronization Disabled")
                 return
