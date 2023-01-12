faaba529741b924b67a839f536c16344ec6fa7d8
Vicente Moreno <vjmoreno@uc.cl>
2023-01-12 09:16:57 -0300

    [ADD] Venta Producto y Servicio

--- src/custom-addons/ccu_connector_esb_account_out/models/crm_team.py
+++ src/custom-addons/ccu_connector_esb_account_out/models/crm_team.py
@@ -6,4 +6,5 @@ class Team(models.Model):
     _inherit = "crm.team"
 
     branch_ccu_code = fields.Char(string='Branch CCU Code')
-    profit_center_code = fields.Char(string='Profit Center Code', index=True)
+    profit_center_code = fields.Char(string='Profit center code for products', index=True)
+    profit_center_code_services = fields.Char(string='Profit center code for services', index=True)
