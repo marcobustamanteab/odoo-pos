1a09e06667c6587e0ffe6cfc40a854a612127f92
vjmoreno <vicente.moreno@psgrupohunting.cl>
2023-03-14 10:02:13 -0300

    [UPD] Cambios para venta a cuenta de

--- src/custom-addons/ccu_pos/models/account_move.py
+++ src/custom-addons/ccu_pos/models/account_move.py
@@ -6,6 +6,8 @@ class AccountMove(models.Model):
     _description = 'Account Move or Invoice'
     _inherit = ['account.move']
 
+    principal_company = fields.Many2one('res.company', string="Principal company")
+
     def _default_sequence_prefix(self):
         if len(self.pos_order_ids) > 0:
             pos_order = self.pos_order_ids[0]
@@ -38,32 +40,6 @@ class AccountMove(models.Model):
             rec.departure_address = "%s - %s" % (address_partner.street, address_partner.street2 or '',)
             rec.departure_city = address_partner.city
             rec.departure_state = address_partner.state_id.name
-
-    # def _compute_pos_sequence_prefix(self):
-    #     for rec in self:
-    #         rec.pos_sequence_prefix = ''
-    #         print(["RESETING", rec.name])
-    #         if self.pos_order_id:
-    #             rec.pos_sequence_prefix = self.pos_order_id.session_id.config_id.sequence_id.prefix.strip('/')
-    #         else:
-    #             if self.pos_session_id:
-    #                 rec.pos_sequence_prefix = self.pos_session_id.config_id.sequence_id.prefix.strip('/')
-    #         if not rec.pos_sequence_prefix:
-    #             pos_order = rec.env['pos.order'].search([('name', '=ilike', rec.payment_reference)], limit=1)
-    #             if pos_order:
-    #                 rec.pos_order_id = pos_order[0].id
-    #                 rec.pos_session_id = pos_order[0].session_id.id
-    #             pos_session = rec.env['pos.session'].search([('name', '=ilike', rec.ref)], limit=1)
-    #             if pos_session:
-    #                 rec.pos_session_id = pos_session[0].id
-    #         if not rec.pos_sequence_prefix:
-    #             if len(rec.pos_order_ids) > 0:
-    #                 pos_order = rec.pos_order_ids[0]
-    #                 if pos_order:
-    #                     rec.pos_order_id = pos_order[0].id
-    #                     rec.pos_session_id = pos_order[0].session_id.id
-    #         if rec.pos_order_id or rec.pos_session_id:
-    #             rec._compute_pos_sequence_prefix()
 
     def reset_cashier_prefix(self):
         moves = self.env['account.move'].search([])
--- src/custom-addons/ccu_pos/models/pos_order.py
+++ src/custom-addons/ccu_pos/models/pos_order.py
@@ -6,6 +6,14 @@ class PosOrder(models.Model):
     _description = 'Point of Sale Order'
     _inherit = ['pos.order']
 
+    account_moves = fields.One2many('account.move', 'pos_order_id', string="Account moves")
+    account_moves_count = fields.Integer('Number of account moves', compute= '_compute_account_moves_count')
+
+    def _compute_account_moves_count(self):
+        for rec in self:
+            account_moves_count = self.env['account.move'].search_count([('pos_order_id', '=', rec.id)])
+            rec.account_moves_count = account_moves_count
+
     def _default_sequence_prefix(self):
         if self.session_id.config_id.sequence_id.prefix:
             return self.session_id.config_id.sequence_id.prefix.strip('/')
@@ -37,3 +45,53 @@ class PosOrder(models.Model):
     def reset_cashier_prefix(self):
         moves = self.env['pos.order'].search([])
         moves._compute_sequence_prefix()
+
+    def group_account_move_lines(self, move_vals):
+        # crear metodo para agrupar los movimientos por empresa mandante
+        return [move_vals]
+
+    def _generate_pos_order_invoice(self):
+        moves = self.env['account.move']
+
+        for order in self:
+            if order.account_move:
+                moves += order.account_move
+                continue
+
+            if not order.partner_id:
+                raise UserError(_('Please provide a partner for the sale.'))
+
+            move_vals = order._prepare_invoice_vals()
+            grouped_vals = self.group_account_move_lines(move_vals)
+            for vals in grouped_vals:
+                new_move = order._create_invoice(vals)
+                new_move.sudo().with_company(order.company_id)._post()
+                moves += new_move
+            if moves:
+                order.write({'account_move': moves[0].id, 'state': 'invoiced'})
+
+        if not moves:
+            return {}
+
+        # cambiar respuesta
+        return {
+            'name': _('Customer Invoice'),
+            'view_mode': 'form',
+            'view_id': self.env.ref('account.view_move_form').id,
+            'res_model': 'account.move',
+            'context': "{'move_type':'out_invoice'}",
+            'type': 'ir.actions.act_window',
+            'nodestroy': True,
+            'target': 'current',
+            'res_id': moves and moves.ids[0] or False,
+        }
+
+    def action_account_moves(self):
+        return {
+            'name': _('Pos order invoices'),
+            'view_mode': 'tree,form',
+            'res_model': 'account.move',
+            'domain': [('pos_order_id', '=', self.id)],
+            'type': 'ir.actions.act_window',
+            'target': 'current',
+        }
