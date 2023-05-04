ad394fceb61432ab992363b9599ee5818131dfe8
vjmoreno <vicente.moreno@psgrupohunting.cl>
2023-05-04 11:27:03 -0400

    [ADD] módulo ccu_sql_export_excel

---
+++ src/custom-addons/ccu_sql_export_excel/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "CCU - SQL Export Excel",
+    'summary': """CCU - SQL Export Excel""",
+    'description': """SQL queries to export data in excel files""",
+    'author': "CCU",
+    'website': "",
+    'category': 'customizations',
+    'version': '1',
+    'depends': ['sql_export_excel'],
+    'data': ['data/ir_model_fields.xml',
+             'data/sql_export.xml'],
+    'installable': True,
+    'application': False
+}
